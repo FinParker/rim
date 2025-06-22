60c1957a3cc9c28c15cc2cb2dbb63746c187ff2a
iming <2576226012@qq.com>
2025-06-22 19:28:27 +0800

    fix: fix release bug

--- src/editor.rs
+++ src/editor.rs
@@ -2,7 +2,7 @@
  * @Author: iming 2576226012@qq.com
  * @Date: 2025-05-01 08:52:36
  * @LastEditors: iming 2576226012@qq.com
- * @LastEditTime: 2025-06-22 16:35:16
+ * @LastEditTime: 2025-06-22 19:28:00
  * @FilePath: \rim\src\editor.rs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -123,7 +123,7 @@ impl Editor {
                     }
                     #[cfg(not(debug_assertions))]
                     {
-                        log::error!("Read event error (ignored): {}", err); // 添加日志记录
+                        println!("Read event error (ignored): {}", err); // 添加日志记录
                     }
                 }
             }
